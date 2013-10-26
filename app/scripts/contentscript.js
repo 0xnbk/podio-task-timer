b0be4df0ef650f77c36a45725d63a10c30598d54
Nikhil Ben Kuruvilla <nikkibk@gmail.com>
2013-10-26 16:40:59 +0530

    Basic structure for the project content script file

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,27 @@
-'use strict';
+$(function (){
+    'use strict';
 
+    var ptTimer = {
+
+        api : {
+           url : "http://phases.nikhilben.com/pt_timer/index.php"
+        },
+        podio : {
+            taskAttribute : ""
+
+
+        },
+
+        init : function () {
+
+        },
+
+        ajaxRequest : function () {
+
+        }
+    };
+
+ptTimer.init();
+
+});
 
