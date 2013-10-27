1a5ef191929a26c5d1962c0c1d8d649ef02b0430
Nikhil Ben Kuruvilla <nikkibk@gmail.com>
2013-10-27 09:06:12 +0530

    Function to get all task id of the tasks displayed in the page

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -7,17 +7,29 @@ $(function (){
            url : "http://phases.nikhilben.com/pt_timer/index.php"
         },
         podio : {
-            taskAttribute : ""
+            taskAttribute : "data-task-id",
+            taskClass : "task-wrapper"
 
 
         },
 
         init : function () {
 
+            var taskArray = ptTimer.getAllTaskID();
+
+            console.log(taskArray);
+
         },
 
         ajaxRequest : function () {
 
+        },
+
+        getAllTaskID : function () {
+            var podio = ptTimer.podio;
+
+            // Get all task ID in the page as an array
+            return $("."+podio.taskClass).map(function(){return $(this).attr(podio.taskAttribute);}).get();
         }
     };
 
