cb626595b024d8c6d587a345f84453cfdb9184fd
Nikhil Ben Kuruvilla <nikkibk@gmail.com>
2013-10-27 10:18:17 +0530

    Added common ajax request function and callFunction methods

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -3,14 +3,15 @@ $(function (){
 
     var ptTimer = {
 
+        // REST API Server
         api : {
            url : "http://phases.nikhilben.com/pt_timer/index.php"
         },
+
+        // Podio settings
         podio : {
             taskAttribute : "data-task-id",
             taskClass : "task-wrapper"
-
-
         },
 
         init : function () {
@@ -21,15 +22,40 @@ $(function (){
 
         },
 
-        ajaxRequest : function () {
+        // Call a function passed through param
+        callFunction: function () {
 
+            var fn = arguments[0],
+                vars = Array.prototype.slice.call(arguments, 1);
+
+            return fn.apply(this, vars);
+        },
+
+        ajaxRequest : function (type, url, data, success) {
+            // Ajax request to server
+            $.ajax({
+                type: type,
+                url: url,
+                data: data,
+                success: function (val) {
+
+                    // Calls the function passed through argument
+                    ptTimer.callFunction(success, val, data);
+                },
+                error: function (xhr) {
+
+
+                }
+            });
         },
 
         getAllTaskID : function () {
             var podio = ptTimer.podio;
 
             // Get all task ID in the page as an array
-            return $("."+podio.taskClass).map(function(){return $(this).attr(podio.taskAttribute);}).get();
+            return $("."+podio.taskClass).map( function () {
+                return $(this).attr(podio.taskAttribute);
+            }).get();
         }
     };
 
